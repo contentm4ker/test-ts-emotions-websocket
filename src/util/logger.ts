91a89977a996d85605d347accf9ca563e329fa38
Grigoriev Ivan <grigorievivan.1@mail.ru>
2021-07-23 17:56:28 +0300

    fix(logger): remove transport to file

--- src/util/logger.ts
+++ src/util/logger.ts
@@ -4,8 +4,7 @@ const options: winston.LoggerOptions = {
     transports: [
         new winston.transports.Console({
             level: process.env.NODE_ENV === "production" ? "error" : "debug"
-        }),
-        new winston.transports.File({ filename: "debug.log", level: "debug" })
+        })
     ]
 };
 
