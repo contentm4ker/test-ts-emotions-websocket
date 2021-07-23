a2eee7c2dc536314811ad4a763a199058f709748
Grigoriev Ivan <grigorievivan.1@mail.ru>
2021-07-23 17:57:20 +0300

    feat(server): add socket emotion things

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,10 @@
+import _ from "lodash";
+import { Server as SocketServer } from "socket.io";
 import errorHandler from "errorhandler";
-import app from "./app";
+
+import app, { baseUrl } from "./app";
+import logger from "./util/logger";
+import { EMOTION_ID_LOWER, EMOTION_ID_UPPER } from "./util/secrets";
 
 
 /**
@@ -20,6 +25,21 @@ const server = app.listen(app.get("port"), () => {
         app.get("env")
     );
     console.log("  Press CTRL-C to stop\n");
+});
+
+const socketIO = new SocketServer(server, { path: `${baseUrl}/socket.io`, transports: ["websocket"] });
+
+socketIO.on("connection", (socket) => {
+
+    socket.on("emotion", (emotionId: number) => {
+        if (!_.isNumber(emotionId) || emotionId < EMOTION_ID_LOWER || emotionId > EMOTION_ID_UPPER) {
+            const errMsg = `Validation Error. Bad emotion '${emotionId}' (${typeof emotionId}). Valid emotions are integers from [1, 8]`;
+            logger.error(errMsg);
+            socket.emit("exception", { msg: errMsg });
+            return;
+        }
+        socket.broadcast.emit("emotion", emotionId);
+    });
 });
 
 export default server;
